834fd94cdeec3aab1ddcc49bc0787f5c44df196f
VirgushAA <126845952+VirgushAA@users.noreply.github.com>
2025-12-19 12:50:41 +0700

    some tables struct + permissions

--- models/user.py
+++ models/user.py
@@ -1,7 +1,21 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Table
 from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
+
+role_permissions = Table(
+    "role_permissions",
+    Base.metadata,
+    Column("role_id", ForeignKey("roles.id"), primary_key=True),
+    Column("permission_id", ForeignKey("permissions.id"), primary_key=True),
+)
+
+class UserRole(Base):
+    __tablename__ = 'user_roles'
+
+    user_id = Column(ForeignKey('users.id'), primary_key=True)
+    role_id = Column(ForeignKey('roles.id'), primary_key=True)
+
 
 class User(Base):
     __tablename__ = 'users'
@@ -28,14 +42,40 @@ class Role(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True, nullable=False)
 
+    permissions = relationship(
+        "Permission",
+        # secondary=role_permissions,
+        secondary=UserRole.__table__,
+        back_populates="roles"
+    )
+
     users = relationship(
         "User",
         secondary="user_roles",
         back_populates="roles"
     )
 
-class UserRole(Base):
-    __tablename__ = 'user_roles'
 
-    user_id = Column(ForeignKey('users.id'), primary_key=True)
-    role_id = Column(ForeignKey('roles.id'), primary_key=True)
+# --------------------------------------
+class Permission(Base):
+    __tablename__ = "permissions"
+
+    id = Column(Integer, primary_key=True)
+    code = Column(String, unique=True)   # "post.delete"
+    resource = Column(String)
+    action = Column(String)
+
+    roles = relationship(
+        "Role",
+        secondary=role_permissions,
+        back_populates="permissions"
+    )
+
+
+    # ------------------------------- попробую для ACL
+# class RolePermission(Base):
+#     __tablename__ = "role_permissions"
+#
+#     role_id = Column(ForeignKey("roles.id"), primary_key=True)
+#     permission_id = Column(ForeignKey("permissions.id"), primary_key=True)
+#     granted_at = Column(DateTime, default=datetime.utcnow)
